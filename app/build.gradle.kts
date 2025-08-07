c2a90ac13cb79e9e196bca9f41064f1316be2f1c
Eitan Adler <lists@eitanadler.com>
2025-08-07 14:44:31 -0400

    [fastlane] bump version code to 10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,7 +47,7 @@ android {
         applicationId = "com.brokenkernel.introspection"
         minSdk = 26
         targetSdk = 36
-        versionCode = 9
+        versionCode = 10
         versionName = "0.0.$versionCode"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
