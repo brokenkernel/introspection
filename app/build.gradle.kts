048e533f97e6e869d2ef104d8edb486682bbd7ac
Eitan Adler <lists@eitanadler.com>
2025-08-07 14:48:31 -0400

    [fastlane] bump version code to 13

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,7 +47,7 @@ android {
         applicationId = "com.brokenkernel.introspection"
         minSdk = 26
         targetSdk = 36
-        versionCode = 12
+        versionCode = 13
         versionName = "0.0.$versionCode"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
