7150b124645363d7904e685b2fa0763a6ebf4c39
Eitan Adler <lists@eitanadler.com>
2025-08-06 21:03:09 -0400

    [lint] add initial lint

--- app/features/coreintrospection/src/main/java/com/brokenkernel/introspection/view/Outer.kt
+++ app/features/coreintrospection/src/main/java/com/brokenkernel/introspection/view/Outer.kt
@@ -1,7 +1,17 @@
 package com.brokenkernel.introspection.view
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @Composable
-public fun OuterContentForCoreIntrospection() {
+public fun OuterContentForCoreIntrospection(modifier: Modifier = Modifier) {
+    Column(modifier) {
+        Text(text = "Hallo")
+        Text(text = "Hallo")
+        Text(text = "Hallo")
+        Text(text = "Hallo")
+        Text(text = "Hallo")
+    }
 }
--- app/src/main/java/com/brokenkernel/introspection/MainActivity.kt
+++ app/src/main/java/com/brokenkernel/introspection/MainActivity.kt
@@ -7,11 +7,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.brokenkernel.introspection.ui.theme.IntrospectionTheme
+import com.brokenkernel.introspection.view.OuterContentForCoreIntrospection
 
 public class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,31 +18,9 @@ public class MainActivity : ComponentActivity() {
         setContent {
             IntrospectionTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding),
-                    )
+                    OuterContentForCoreIntrospection(Modifier.padding(innerPadding))
                 }
             }
         }
-    }
-}
-
-@Composable
-public fun Greeting(
-    name: String,
-    modifier: Modifier = Modifier,
-) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier,
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-public fun GreetingPreview() {
-    IntrospectionTheme {
-        Greeting("Android")
     }
 }
