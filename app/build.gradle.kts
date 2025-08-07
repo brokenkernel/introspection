94e68e8c7310bc3d38cd2245fe2986ec4872b285
Eitan Adler <lists@eitanadler.com>
2025-08-07 01:17:33 -0400

    [build] bump to versioncode 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         applicationId = "com.brokenkernel.introspection"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
+        versionCode = 2
         versionName = "0.0.$versionCode"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
