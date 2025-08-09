60a7aee10a2843ef0baf869787c5cf8fc16b80ed
Eitan Adler <lists@eitanadler.com>
2025-08-09 10:23:34 -0400

    [all] clean up minimum API now that I'm moving to reflective approach

--- app/src/main/java/com/brokenkernel/introspection/coreintrospection/BuildInfoTab.kt
+++ app/src/main/java/com/brokenkernel/introspection/coreintrospection/BuildInfoTab.kt
@@ -8,7 +8,6 @@ import androidx.compose.material3.ListItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.platform.Clipboard
 import androidx.compose.ui.platform.LocalClipboard
 import androidx.compose.ui.platform.LocalConfiguration
@@ -69,32 +68,4 @@ internal fun BuildInfoTab(modifier: Modifier = Modifier) {
             )
         }
     }
-}
-
-@Composable
-private fun MinimumAPIListItemVersionS(
-    apiLevel: Int,
-    modifier: Modifier = Modifier,
-    datum: @Composable () -> Unit,
-) {
-    val isValid = Build.VERSION.SDK_INT >= apiLevel
-    val currentModifier = if (isValid) {
-        modifier
-    } else {
-        Modifier.alpha(0.5f) // maybe a better way to describe "disabled" ?
-    }
-    ListItem(
-        headlineContent = {
-            Text("ODM_SKU")
-        },
-        supportingContent = {
-            if (isValid) {
-                datum()
-            }
-        },
-        trailingContent = {
-            Text("Minimum API ${Build.VERSION_CODES.S}")
-        },
-        modifier = currentModifier,
-    )
 }
--- app/src/main/java/com/brokenkernel/introspection/coreintrospection/Camera2Tab.kt
+++ app/src/main/java/com/brokenkernel/introspection/coreintrospection/Camera2Tab.kt
@@ -1,13 +1,10 @@
 package com.brokenkernel.introspection.coreintrospection
 
-import android.os.Build
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material3.ListItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 
 @Composable
 internal fun Camera2Tab(modifier: Modifier = Modifier) {
@@ -20,32 +17,4 @@ internal fun Camera2Tab(modifier: Modifier = Modifier) {
             Text("Camera Data")
         }
     }
-}
-
-@Composable
-private fun MinimumAPIListItemVersionS(
-    apiLevel: Int,
-    modifier: Modifier = Modifier,
-    datum: @Composable () -> Unit,
-) {
-    val isValid = Build.VERSION.SDK_INT >= apiLevel
-    val currentModifier = if (isValid) {
-        modifier
-    } else {
-        Modifier.alpha(0.5f) // maybe a better way to describe "disabled" ?
-    }
-    ListItem(
-        headlineContent = {
-            Text("ODM_SKU")
-        },
-        supportingContent = {
-            if (isValid) {
-                datum()
-            }
-        },
-        trailingContent = {
-            Text("Minimum API ${Build.VERSION_CODES.S}")
-        },
-        modifier = currentModifier,
-    )
 }
