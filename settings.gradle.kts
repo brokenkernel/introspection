bffc3967f5b324520edc0a9f2f289be4bffe0d71
Eitan Adler <lists@eitanadler.com>
2026-03-03 17:33:15 -0800

    [build] remove kover since it causes problems and I don't use it

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,17 +19,8 @@ dependencyResolutionManagement {
     }
 }
 
-plugins {
-    id("org.jetbrains.kotlinx.kover.aggregation") version "0.9.1"
-}
-
 enableFeaturePreview("STABLE_CONFIGURATION_CACHE")
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
-
-kover {
-    enableCoverage()
-}
-
 
 rootProject.name = "introspection"
 include(":app")
